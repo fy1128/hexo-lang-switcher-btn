a5257a52b82e2e095ae437eb062e02913c44449c
fy1128 <892973009@qq.com>
2019-08-21 01:41:22 +0800

    updated.

---
+++ README.md
@@ -0,0 +1,13 @@
+## Useage
+
+### added this to _config.yml
+
+``` yml
+langswitch:
+  en: Read the English version
+  tw: 閱讀中文版
+```
+
+### added to your post
+
+{% langswitch en %}
--- index.js
+++ index.js
@@ -6,5 +6,5 @@ hexo.extend.tag.register('langswitch', function(args, content) {
   var path = this.path
   var lang = args[0];
   var permalink = '/' + lang + '/' + path;
-  return '<a class="btn" href="' + permalink + '"><i class="fa fa-language fa-fw"></i>' + hexo.config.alangbtn[lang] + '</a>';   
+  return '<a class="btn" href="' + permalink + '"><i class="fa fa-language fa-fw"></i>' + hexo.config.langswitch[lang] + '</a>';   
 }, -999);
